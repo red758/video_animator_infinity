c93a55db2bfa265a467da72db2392f5193666cb1
red758 <red095821@gmail.com>
2026-02-19 19:03:39 +0530

    Update FileUpload.tsx

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -11,7 +11,7 @@ const SAMPLES = [
   { 
     id: '1', 
     name: 'THE DREAM', 
-    url: 'deep_space.mp4', 
+    url: 'https://storage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4', 
     icon: <Rocket size={32}/>,
     tag: 'SURREAL'
   },
