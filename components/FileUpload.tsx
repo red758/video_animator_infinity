2fc632975d0754876d1d65fca7acd89c0dd95b34
red758 <red095821@gmail.com>
2026-02-28 17:21:25 +0530

    Update video URL for THE DREAM sample

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -11,7 +11,7 @@ const SAMPLES = [
   { 
     id: '1', 
     name: 'THE DREAM', 
-    url: 'https://player.cloudinary.com/embed/?cloud_name=dd7o282ls&public_id=space_jupuge.mp4', 
+    url: 'https://res.cloudinary.com/dd7o282ls/video/upload/v1771513308/space_jupuge.mp4', 
     icon: <Rocket size={32}/>,
     tag: 'SURREAL'
   },
